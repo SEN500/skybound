37c456415951fc3adfa7e7c9762343784eb9e8e4
SEN500 <givarave99@gmail.com>
2025-09-07 17:07:27 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,21 @@
+import BootScene from './scenes/BootScene.js';
+import PlayScene from './scenes/PlayScene.js';
+
+const config = {
+  type: Phaser.AUTO,
+  width: 960,
+  height: 540,
+  parent: 'game',
+  backgroundColor: '#7fc8ff',
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 1000 },
+      debug: false
+    }
+  },
+  scene: [BootScene, PlayScene]
+};
+
+window.game = new Phaser.Game(config);
 
