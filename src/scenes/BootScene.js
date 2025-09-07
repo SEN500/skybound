5fe74cd081ade276468306a7701381e5213515f0
SEN500 <givarave99@gmail.com>
2025-09-07 17:07:54 +0100

    Update BootScene.js

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -1,1 +1,10 @@
+export default class BootScene extends Phaser.Scene {
+  constructor() { super('BootScene'); }
+  preload() {
+    // nothing to load: we generate small graphics at runtime
+  }
+  create() {
+    this.scene.start('PlayScene');
+  }
+}
 
